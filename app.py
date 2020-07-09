133b56b0142b509527180a823f1624860e40ffd7
Rob <rlau929@gmail.com>
2020-07-08 20:43:55 -0700

    Adding app.py file

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ app = Flask(__name__)
 def welcome():
     return(
     '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
+    Welcome to the Climate Analysis API!<br>
+    Available Routes:<br>
+    /api/v1.0/precipitation<br>
+    /api/v1.0/stations<br>
+    /api/v1.0/tobs<br>
     /api/v1.0/temp/start/end
     ''')
 
@@ -59,15 +59,14 @@ def temp_monthly():
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
-    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
-    if not end: 
-	    results = session.query(*sel).\
-	filter(Measurement.date <= start).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps)
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
     results = session.query(*sel).\
-    filter(Measurement.date >= start).\
-	filter(Measurement.date <= end).all()
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
     return jsonify(temps=temps)
- 
